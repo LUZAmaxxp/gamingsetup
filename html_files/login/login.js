e5c11b6d3f75f54bceb5a46228d73a432c4ae757
LUZAmaxxp <ayman.allouch@e-polytechnique.ma>
2024-02-10 18:42:56 +0100

    feat:added a login form checking your email and redirecting to home page

---
+++ html_files/login/login.js
@@ -0,0 +1,33 @@
+function checkin(email, password) {
+  let text;
+  let PasswordRegex = /^[A-Za-z]\w{7,14}$/;
+  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailRegex.test(email) && PasswordRegex.test(password)) {
+    text = " Welcome !";
+    document.getElementById("bemo").innerHTML = text;
+    window.location.href = "../index.html";
+    return true;
+  } else {
+    text = "please enter a valid email and password";
+    document.getElementById("bemo").innerHTML = text;
+    return false;
+  }
+}
+function addUser(event) {
+  event.preventDefault();
+  let email = document.getElementById("numb").value;
+  let password = document.getElementById("emb").value;
+  if (checkin(email, password)) {
+    let myuser = JSON.parse(localStorage.getItem("userData"));
+    if (myuser === null) {
+      let userData = [];
+      userData.push({ email, password });
+      localStorage.setItem("userData", JSON.stringify(userData));
+    } else {
+      myuser.push({ email, password });
+      localStorage.setItem("userdata", JSON.stringify(myuser));
+    }
+  } else {
+    return "failed to add user";
+  }
+}
